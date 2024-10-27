5e43b4d8d0b6e43cc003f974a57df50234660200
HuuNhan1308 <nhanhohuunhan7398@gmail.com>
2024-10-27 13:27:04 +0700

    Feat(Student): As a student, I want to see my pending recruitment request

--- src/main/java/com/intern/app/mapper/RecruitmentRequestMapper.java
+++ src/main/java/com/intern/app/mapper/RecruitmentRequestMapper.java
@@ -2,11 +2,16 @@ package com.intern.app.mapper;
 
 import com.intern.app.models.dto.request.RecruitmentCreationRequest;
 import com.intern.app.models.dto.request.RecruitmentRequestCreationRequest;
+import com.intern.app.models.dto.response.InstructorRequestResponse;
+import com.intern.app.models.entity.InstructorRequest;
 import com.intern.app.models.entity.Recruitment;
 import com.intern.app.models.entity.RecruitmentRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface RecruitmentRequestMapper {
     RecruitmentRequest toRecruitmentRequest(RecruitmentRequestCreationRequest recruitmentRequestCreationRequest);
 }
+
--- src/main/java/com/intern/app/repository/InstructorRequestRepository.java
+++ src/main/java/com/intern/app/repository/InstructorRequestRepository.java
@@ -5,13 +5,17 @@ import com.intern.app.models.entity.Instructor;
 import com.intern.app.models.entity.InstructorRequest;
 import com.intern.app.models.enums.RequestStatus;
 import com.intern.app.repository.CustomRepository.AppRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface InstructorRequestRepository extends AppRepository<InstructorRequest, String> {
+public interface InstructorRequestRepository extends AppRepository<InstructorRequest, String>, JpaSpecificationExecutor<InstructorRequest> {
     Optional<InstructorRequest> findByStudentStudentIdAndInstructorInstructorId(String studentId, Instructor instructor);
     Optional<InstructorRequest> findByInstructorRequestId(String instructorRequestId);
 
