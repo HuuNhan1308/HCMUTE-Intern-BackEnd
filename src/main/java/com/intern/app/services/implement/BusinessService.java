0b0d90a1a80a1c00e3bd55af8a8135fc1404deb8
HuuNhan1308 <nhanhohuunhan7398@gmail.com>
2024-11-20 11:49:28 +0700

    Update BusinessService.java

--- src/main/java/com/intern/app/services/implement/BusinessService.java
+++ src/main/java/com/intern/app/services/implement/BusinessService.java
@@ -131,7 +131,7 @@ public class BusinessService implements IBusinessService {
             NotificationRequest notificationRequest = NotificationRequest.builder()
                     .read(false)
                     .title("Yêu cầu thực tập của bạn đã có kết quả")
-                    .content("yêu cầu đến bài tuyển dụng " + recruitmentRequest.getRecruitment().getTitle() + " đã được " + messageResult)
+                    .content("Yêu cầu đến bài tuyển dụng " + recruitmentRequest.getRecruitment().getTitle() + " đã được " + messageResult)
                     .ownerId(recruitmentRequest.getRecruitment().getBusiness().getManagedBy().getProfileId())
                     .profileId(recruitmentRequest.getStudent().getProfile().getProfileId())
                     .build();
